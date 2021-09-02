6e34742856612624bdc61ceed3c77ec29678fed2
propil5 <pilch_piotr@outlook.com>
2021-09-03 00:44:45 +0200

    Removing usage of azure vault as it is causing a build issue in github action.

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Azure.Identity;
+//using Azure.Identity;
 
 namespace WarelogManager.Server
 {
@@ -19,11 +15,11 @@ namespace WarelogManager.Server
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((context, config) =>
-            {
-            var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-            config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-            })
+                //.ConfigureAppConfiguration((context, config) =>
+                //{
+                //var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+                //config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+                //})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
