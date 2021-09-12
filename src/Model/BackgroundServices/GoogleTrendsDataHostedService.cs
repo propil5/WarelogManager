d2f429b07504a46061a26910bc589ebfc118d598
propil5 <pilch_piotr@outlook.com>
2021-09-12 23:56:29 +0200

    WIP: Adding the background service to pull data from google trends using python scripts.

---
+++ src/Model/BackgroundServices/GoogleTrendsDataHostedService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NCrontab;
+
+namespace WarelogManager.Model.BackgroundServices
+{
+    public class GoogleTrendsDataHostedService : BackgroundService
+    {
+        private CrontabSchedule _schedule;
+        private DateTime _nextRun;
+        private string Schedule => "0 0 * ? * *	"; //Runs every hour
+
+        public GoogleTrendsDataHostedService()
+        {
+            _schedule = CrontabSchedule.Parse(Schedule, new CrontabSchedule.ParseOptions
+            { IncludingSeconds = true });
+            _nextRun = _schedule.GetNextOccurrence(DateTime.Now);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            do
+            {
+                var now = DateTime.Now;
+                if (now > _nextRun)
+                {
+                    Process();
+                    _nextRun = _schedule.GetNextOccurrence(DateTime.Now);
+                }
+                await Task.Delay(5000, stoppingToken); //5 seconds delay
+            }
+            while (!stoppingToken.IsCancellationRequested);
+        }
+
+        private void Process()
+        {
+            Console.WriteLine("hello world" + DateTime.Now.ToString("F"));
+        }
+    }
+}
---
+++ src/Model/Python/GoogleTrending.py
@@ -0,0 +1,5 @@
+﻿from pytrends.request import TrendReq
+import numpy as np
+import sys
+
+pytrends = TrendReq(hl='pl-PL', tz=360)
