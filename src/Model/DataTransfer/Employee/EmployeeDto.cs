5c99aa2e28c16f68b98520a7b4134af90e3d1a23
propil5 <pilch_piotr@outlook.com>
2021-09-24 23:49:46 +0200

    WIP: Employment domain.

---
+++ src/Model/DataTransfer/Employee/EmployeeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarelogManager.Model.DataTransfer.Common;
+using WarelogManager.Model.DataTransfer.Employee;
+using WarelogManager.Model.DataTransfer.Utilities;
+
+namespace WarelogManager.Model.DataTransfer.NewFolder
+{
+    public class EmployeeDtoc : BaseEntity
+    {
+        public int JobPositionDtoId { get; set; }
+        public JobPositionDto JobPosition { get; set; }
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public int EmploymentContractId { get; set; }
+        public EmploymentContractDto EmploymentContract { get; set; }
+    }
+}
