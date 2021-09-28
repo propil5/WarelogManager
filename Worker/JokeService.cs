761c4d8d97932f3698964244fccd6aee8fd4fc8b
propil5 <pilch_piotr@outlook.com>
2021-09-28 23:56:23 +0200

    Worker service initial with docker support

---
+++ Worker/JokeService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Worker.Model;
+
+namespace Worker
+{
+    public class JokeService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly JsonSerializerOptions _options = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        private const string JokeApiUrl =
+            "https://official-joke-api.appspot.com/jokes/programming/random";
+
+        public JokeService(HttpClient httpClient) => _httpClient = httpClient;
+
+        public async Task<string> GetJokeAsync()
+        {
+            try
+            {
+                // The API returns an array with a single entry.
+                Joke[]? jokes = await _httpClient.GetFromJsonAsync<Joke[]>(
+                    JokeApiUrl, _options);
+
+                Joke? joke = jokes?[0];
+
+                return joke is not null
+                    ? $"{joke.Setup}{Environment.NewLine}{joke.Punchline}"
+                    : "No joke here...";
+            }
+            catch (Exception ex)
+            {
+                return $"That's not funny! {ex}";
+            }
+        }
+    }
+}
---
+++ Worker/Model/Joke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker.Model
+{
+    public class Joke
+    {
+        public string Setup { get; set; }
+        public string Punchline { get; set; }
+    }
+}
